7048f4799b2fb51d85aea138f57e929fdb60af45
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-04-06 16:53:47 -0300

    Restify configured to parse JSON on the req.body

---
+++ application/src/server.js
@@ -0,0 +1,22 @@
+import restify from 'restify'
+import Routes from './routes'
+
+const PORT = process.env.PORT || 3000
+
+const start = () => {
+  const server = restify.createServer()
+  server.use(restify.plugins.bodyParser())
+
+  // Initialize every route
+  for (const route of Routes) {
+    route.initialize(server)
+  }
+
+  // Start server
+  server.listen(PORT, () => console.log(`Server listening on port ${PORT}.`))
+}
+
+export default {
+  PORT,
+  start
+}
