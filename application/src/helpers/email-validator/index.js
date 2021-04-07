9fc1fbec6f58fc631732000a6befe8d585ea6edf
VeigaCoder <gbrl316@gmail.com>
2021-04-07 13:16:32 -0300

    Fixing comment error

--- application/src/helpers/email-validator/index.js
+++ application/src/helpers/email-validator/index.js
@@ -1,7 +1,7 @@
 // Requires npm package 'email-validator'
 import EMAIL from 'email-validator'
 
-// Exports the 'validate' functionality from cpf-check
+// Exports the 'validate' functionality from rmail-validator
 export const validation = email => EMAIL.validate(email)
 
 // Exports desconstructed
