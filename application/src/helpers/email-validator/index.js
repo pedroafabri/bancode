38e9bad9e4593014899a3560f3ac7bbf9f57cbca
VeigaCoder <gbrl316@gmail.com>
2021-04-07 13:17:35 -0300

    Fixing again

--- application/src/helpers/email-validator/index.js
+++ application/src/helpers/email-validator/index.js
@@ -1,7 +1,7 @@
 // Requires npm package 'email-validator'
 import EMAIL from 'email-validator'
 
-// Exports the 'validate' functionality from rmail-validator
+// Exports the 'validate' functionality from email-validator
 export const validation = email => EMAIL.validate(email)
 
 // Exports desconstructed
