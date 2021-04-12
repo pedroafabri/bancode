0097c81127f7056ff797a2f7a18fd441f09b7d34
Pedro Fabri <pedro.fabri@mzgroup.com>
2021-04-12 16:05:00 -0300

    Fixed tests

--- application/tests/unit/emailSender.test.js
+++ application/tests/unit/emailSender.test.js
@@ -1,17 +1,18 @@
 import EmailSender from '../../src/helpers/emailSender'
 
 describe('EmailSender tests', () => {
-  it('Should throw error when not passing user e-mail', () => {
-    const send = () => EmailSender.sendWelcomeEmail()
-    expect(send).toThrow(Error)
+  it('Should throw error when not passing user e-mail', async () => {
+    await expect(EmailSender.sendWelcomeEmail())
+    .rejects
+    .toThrow('email field not filled');
   })
 
-  it('Should send user e-mail even without all info', () => {
+  it('Should send user e-mail even without all info', async () => {
     const send = () => EmailSender.sendWelcomeEmail('pedroafabri@gmail.com')
     expect(send).not.toThrow(Error)
   })
 
-  it('Should send e-mail correctly', () => {
+  it('Should send e-mail correctly', async () => {
     const send = () => EmailSender.sendWelcomeEmail('pedroafabri@gmail.com', {firstName: 'Pedro', lastName: 'Fabri'})
     expect(send).not.toThrow(Error)
   })
