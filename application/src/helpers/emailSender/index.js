116797e3e5418378073859e878b1632928d8c21c
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-05-25 18:51:01 -0300

    fix errors in emailSender helper

--- application/src/helpers/emailSender/index.js
+++ application/src/helpers/emailSender/index.js
@@ -5,76 +5,41 @@ export const sendWelcomeEmail = async ({ email, firstName, lastName }, token) =>
   // conect with the sendgrid api
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-  // throw a err in case the function dont receive a email
-  if (!email) throw new Error('email field not filled')
-
   // sender template
   const message = {
-    from: {
-      email: process.env.EMAIL_SENDER
-    },
-    subject: 'confirmation e-mail',
-    personalizations: [
-      {
-        to: [
-          {
-            email: email
-          }
-        ],
-        dynamic_template_data: {
-          firstname: firstName,
-          lastname: lastName,
-          token: token
-        }
-      }
-    ],
-    template_id: 'd-4fb97b0cd4eb49dda25862a581c781d7'
+    to: email,
+    from: process.env.EMAIL_SENDER,
+    subject: 'confirmation email',
+    template_id: 'd-4fb97b0cd4eb49dda25862a581c781d7',
+    dynamic_template_data: {
+      firstname: firstName,
+      lastname: lastName,
+      token: token
+    }
   }
 
   // try delivering the message, in case it goes wrong throw the  error
-  try {
-    await sgMail.send(message)
-    console.log('Email sent')
-  } catch (error) {
-    console.log(error)
-    throw error
-  }
+  await sgMail.send(message)
+  console.log('Email sent')
 }
 
 export const sendPasswordRecoveryEmail = async ({ email, firstName, lastName }, token) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-  if (!email) throw new Error('email field not filled')
-
   const message = {
-    from: {
-      email: process.env.EMAIL_SENDER
-    },
-    subject: 'recuperação de senha',
-    personalizations: [
-      {
-        to: [
-          {
-            email: email
-          }
-        ],
-        dynamic_template_data: {
-          firstname: firstName,
-          lastname: lastName,
-          token: token
-        }
-      }
-    ],
-    template_id: 'd-63d39b3ef4044aaa9bf8834b52d1c7fa'
+    to: email,
+    from: process.env.EMAIL_SENDER,
+    subject: 'password recovery email',
+    template_id: 'd-63d39b3ef4044aaa9bf8834b52d1c7fa',
+    dynamic_template_data: {
+      firstname: firstName,
+      lastname: lastName,
+      token: token
+    }
   }
 
-  try {
-    await sgMail.send(message)
-    console.log('Email sent')
-  } catch (error) {
-    console.log(error)
-    throw error
-  }
+  await sgMail.send(message)
+  console.log('Email sent')
 }
 
 export default {
