1aec4a90947e29f24c4180cf0185f4b2151045d9
luk-jedi <lucas.zet@hotmail.com>
2021-05-31 16:44:19 -0300

    rotas get

--- application/src/routes.js
+++ application/src/routes.js
@@ -1,8 +1,10 @@
 import { HealthcheckRoutes } from './modules/healthcheck'
 import { UserRoutes } from './modules/users'
+import { TransferRoutes } from './modules/transfer'
 
 // Export every route as an array
 export default [
   UserRoutes,
-  HealthcheckRoutes
+  HealthcheckRoutes,
+  TransferRoutes
 ]
